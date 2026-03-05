6a48216f4b415b5bff6465cecc2e204dbd8f10fa
noviaahln <161816037+noviaahln@users.noreply.github.com>
2026-03-05 10:49:52 +0700

    notif, profill, pembelajaran saya

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <title>LMS - @yield('title', 'word')</title>
+    
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+            const texts = [
+                "Belajar tidak mengenal usia",
+                "Belajar bisa dimulai kapan saja",
+                "Ilmu selalu punya tempat",
+                "Terus belajar, terus berkembang",
+                "Langkah kecil untuk ilmu besar",
+                "Sehat itu sederhana, asal konsisten",
+                "Jangan lupa jaga kesehatan",
+                "Sehat bikin hidup lebih nyaman",
+                "Tubuh sehat, pikiran kuat",
+                "Mulai hari ini, hidup lebih sehat",
+                "Belajar pelan pelan, yang penting jalan",
+                "Kesehatan bukan pilihan, tapi kebutuhan"
+                ];
+
+            const randomText = texts[Math.floor(Math.random() * texts.length)];
+                document.getElementById("dynamicText").innerText = randomText;
+            });
+    </script>
+
+    <script src="//unpkg.com/alpinejs" defer></script>
+
+    <script src="//unpkg.com/alpinejs" defer></script>
+
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('login');
+})->name('login');
+
+Route::post('/', function () {
+    return "Login berhasil (sementara)";
+})->name('login.post');
+
+Route::get('/pembelajaran', function () {
+    return view('pembelajaran');
+});
+
+Route::get('/belum-mulai', function () {
+    return view('belum-mulai');
+});
+
+Route::get('/materi-progress', function () {
+    return view('materi-progress');
+});
+
+Route::get('/materi-selesai', function () {
+    return view('materi-selesai');
+});
